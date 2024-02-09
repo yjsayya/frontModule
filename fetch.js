ee0c2c0a28259be52a6351f8c132b7457ac4a000
sayya <djdj9876@naver.com>
2024-02-09 22:23:37 +0900

    feat: js es6 fetch() 함수

---
+++ fetch.js
@@ -0,0 +1,13 @@
+function getPostLists(currentPage,pageSize) {
+  const url = 'http://localhost:8080';
+  fetch(url)
+      .then(response => {
+          return response.json();
+      })
+      .then(data => {
+          console.log(data);
+      })
+      .catch(error => {
+          console.error('오류 발생:', error);
+      });
+}
